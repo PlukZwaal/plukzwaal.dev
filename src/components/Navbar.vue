1a7e30e60fb8c0afc317ae175d4075aa20d1344e
Pluk Zwaal <40401658+PlukZwaal@users.noreply.github.com>
2024-07-05 14:02:12 +0200

    Created basic nav

---
+++ src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+    <RouterLink to="/projects">Projects</RouterLink>
+    <RouterLink to="/contact">Contact</RouterLink>
+  </header>
+</template>
+
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,13 +6,23 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: () => import('../views/HomeView.vue')
     },
     {
       path: '/about',
       name: 'about',
       component: () => import('../views/AboutView.vue')
-    }
+    },
+    {
+      path: '/projects',
+      name: 'projects',
+      component: () => import('../views/ProjectsView.vue')
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: () => import('../views/ContactView.vue')
+    },
   ]
 })
 
