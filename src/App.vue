22872749f08752c7ae7c084d02b83bf0cd790ac2
Pluk Zwaal <40401658+PlukZwaal@users.noreply.github.com>
2024-07-05 16:05:58 +0200

    Created basic navbar for desktop & mobile!

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@
 
 <template>
   <Navbar />
-
   <RouterView />
 </template>
 
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,14 +1,82 @@
-<script setup>
-</script>
-
 <template>
-  <header>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
-    <RouterLink to="/projects">Projects</RouterLink>
-    <RouterLink to="/contact">Contact</RouterLink>
-  </header>
+    <header>
+        <div class="wrapper">
+            <div class="hamburger" @click="toggleMenu">{{ isMenuOpen ? '✖' : '☰' }}</div>
+            <nav :class="{ open: isMenuOpen }">
+                <RouterLink to="/" @click="closeMenu">Home</RouterLink>
+                <RouterLink to="/about" @click="closeMenu">About</RouterLink>
+                <RouterLink to="/projects" @click="closeMenu">Projects</RouterLink>
+                <RouterLink to="/contact" @click="closeMenu">Contact</RouterLink>
+            </nav>
+        </div>
+    </header>
 </template>
 
-<style>
+<script setup>
+import { ref } from 'vue';
+
+const isMenuOpen = ref(false);
+
+const toggleMenu = () => {
+    isMenuOpen.value = !isMenuOpen.value;
+};
+
+const closeMenu = () => {
+    isMenuOpen.value = false;
+};
+</script>
+
+<style scoped>
+.wrapper {
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 100%;
+}
+
+header {
+    height: 100px;
+}
+
+a {
+    text-decoration: none;
+    color: #000000;
+}
+
+.hamburger {
+    display: none;
+}
+
+nav {
+    display: flex;
+    gap: 20px;
+}
+
+@media (max-width: 768px) {
+    .hamburger {
+        display: block;
+        font-size: 30px;
+        cursor: pointer;
+        z-index: 2;
+    }
+
+    nav {
+        display: none;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(255, 255, 255, 0.75);
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        z-index: 1;
+    }
+
+    nav.open {
+        display: flex;
+    }
+}
 </style>
